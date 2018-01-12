f10818171d7dc6550951aedfcab7bdf9c0fc7a59
Michael Burns <michael@mirwin.net>
2018-01-11 19:05:14 -0800

    try things and stuff

--- endpoint.rb
+++ endpoint.rb
@@ -2,8 +2,13 @@
 
 require 'hanami/router'
 
-app = Hanami::Router.new do
-  get '/', to: ->(env) { [200, {}, ['<p>Hello, World</p>']] }
-end
+app = Hanami::Router.new
+endpoint = ->(_env) { [200, {}, ['<p>Hello, World</p>']] }
+
+router.get    '/', to: endpoint
+router.post   '/', to: endpoint
+
+# TODO : check for the Accept header, respond differently if found:
+# get '/', to: ->(_env) { [200, {"HTTP_ACCEPT"=>"application/json", "CONTENT_TYPE"=>"application/json"}, ['{"message": "Good morning"}']] }
 
 Rack::Server.start app: app, Port: 8080
